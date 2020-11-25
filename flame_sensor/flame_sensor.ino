5deadab09a04c1fd699e3b83bf941e3fa978ec5c
andreea1297 <andreeac12@yahoo.com>
2020-11-25 19:29:56 +0200

    Add files via upload
    Semaphore for tasks

--- flame_sensor/flame_sensor.ino
+++ flame_sensor/flame_sensor.ino
@@ -1,12 +1,23 @@
 #include <Arduino_FreeRTOS.h>
+#include<semphr.h>
 
 void Task_ReadFromSensor(void *pvParameters);
 void Led(void *pvParameters);
 void Taskprint( void *pvParameters );
 
+
+SemaphoreHandle_t xSerialSemaphore;
+
 void setup()
 {
   Serial.begin(9600);
+
+  if (xSerialSemaphore == NULL){
+    xSerialSemaphore = xSemaphoreCreateMutex();
+    if ((xSerialSemaphore) != NULL)
+    xSemaphoreGive((xSerialSemaphore));
+    }
+    
   xTaskCreate(
     Task_ReadFromSensor
     , "Sensor"
@@ -40,14 +51,19 @@ void Task_ReadFromSensor(void *pvParameters)
 {
   pinMode(8, OUTPUT);
   while(1){
+   
   int sensorReading = analogRead(A0); //senzorul e conectat la pinul A0 (pin analog)
   range = map(sensorReading, val_min_sensor, val_max_sensor, 0, 3);
   if (range == 0)
   {
-    Serial.println("Foc");
-    analogWrite (speaker_pin, 50);
+    if (xSemaphoreTake (xSerialSemaphore, (TickType_t) 10) == pdTRUE){
+      Serial.println("Foc");
+      analogWrite (speaker_pin, 50);
+      xSemaphoreGive(xSerialSemaphore);
   }
+   vTaskDelay(1);
   }
+}
 }
 
 void Led(void *pvParameters)  
@@ -55,12 +71,17 @@ void Led(void *pvParameters)
   pinMode(7, OUTPUT);
   while(1)
   {
-    Serial.println("LED aprins-stins (blink)");
-    digitalWrite(7, HIGH);   
-    vTaskDelay( 300 / portTICK_PERIOD_MS ); 
-    digitalWrite(7, LOW);   
-    vTaskDelay( 300 / portTICK_PERIOD_MS ); 
+    if (xSemaphoreTake (xSerialSemaphore, (TickType_t) 5) == pdTRUE){
+     Serial.println("LED aprins-stins (blink)");
+     digitalWrite(7, HIGH);   
+     vTaskDelay( 300 / portTICK_PERIOD_MS ); 
+     digitalWrite(7, LOW);   
+     vTaskDelay( 300 / portTICK_PERIOD_MS ); 
+     xSemaphoreGive(xSerialSemaphore);
   }
+ 
+    vTaskDelay(1);
+}
 }
 
 
@@ -68,7 +89,10 @@ void Taskprint(void *pvParameters)  {
   int counter = 0;
   while(1)
   {
-counter++;
+    if (xSemaphoreTake (xSerialSemaphore, (TickType_t) 5) == pdTRUE){
+  counter++;
   Serial.println(counter); 
+  xSemaphoreGive(xSerialSemaphore);
   vTaskDelay(500 / portTICK_PERIOD_MS);    }
 }
+  vTaskDelay(1);}
